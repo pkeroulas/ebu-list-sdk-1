af369d5ca90c2b110e48903d938cb4c29121bd50
Pedro Ferreira <pedro@bisect.pt>
2021-03-15 19:21:42 +0000

    Update demo (#17)

--- demos/src/demos/login/index.ts
+++ demos/src/demos/login/index.ts
@@ -6,13 +6,8 @@ export const run = async (args: IArgs) => {
     await list.login(args.username, args.password);
     try {
         console.log('Connected');
-        const path = await list.stream.downloadAncillaryUrl(
-            '8f0c2398-4d1e-4e79-b088-d2d2afdeeed9',
-            '0bf6d466-a3c1-4df2-87c3-216b226a5108',
-            '2f0c653d-5e2d-4a26-afd1-db9d7f618e96.raw'
-        );
-        const text = await list.stream.downloadText(path);
-        console.log(JSON.stringify(text));
+        const version = await list.info.getVersion();
+        console.log(JSON.stringify(version));
     } catch (e) {
         console.log(`Error: ${JSON.stringify(e)}`);
     } finally {
