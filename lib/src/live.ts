a52ae210381f41bd1c667a7935d432ac6073297d
Patrick Keroulas <patrick.keroulas@radio-canada.ca>
2021-06-08 15:02:40 -0400

    lib: add awaiter in live

--- lib/src/live.ts
+++ lib/src/live.ts
@@ -1,6 +1,8 @@
+import { ILiveSource } from './types';
+import { PcapFileProcessingDone } from './api/pcap';
 import { Transport } from '@bisect/bisect-core-ts';
 import { Workflow } from './api/constants';
-import { ILiveSource } from './types';
+import { wsEvents } from './api';
 
 //////////////////////////////////////////////////////////////////////////////
 
@@ -11,6 +13,19 @@ export default class Live {
         const response = await this.transport.get('/api/live/sources');
         const sources: ILiveSource[] = response as ILiveSource[];
         return sources;
+    }
+
+    public makeCaptureAwaiter(filename: string, timeoutMs: number): Promise<PcapFileProcessingDone | undefined> {
+        return this.transport.makeAwaiter<PcapFileProcessingDone | undefined>(
+            wsEvents.Pcap.processingDone,
+            (data: any) => {
+                if (data.file_name === filename) {
+                    return data as PcapFileProcessingDone;
+                }
+                return undefined;
+            },
+            timeoutMs
+        );
     }
 
     public async startCapture(filename: string, durationMs: number, sources: string[]) {
