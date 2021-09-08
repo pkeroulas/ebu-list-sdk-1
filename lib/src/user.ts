8a2d569831cb3dfbf9a59deb509336fc54baaab5
Nelson Da Silva <63600033+nelsonsilva94@users.noreply.github.com>
2021-09-08 14:21:37 +0100

    remove gdpr (#44)
    Co-authored-by: Nelson Silva <nelson@bisect.pt>

--- lib/src/user.ts
+++ lib/src/user.ts
@@ -37,14 +37,4 @@ export default class User {
         const response = await this.transport.patch(`/api/user/read-only`, { value });
         return response;
     }
-
-    public async acceptGDPR(data: any): Promise<void> {
-        const response = await this.transport.post(`/api/user/gdpr`, data);
-        return response;
-    }
-
-    public async getGDPRStatus(): Promise<any> {
-        const response = await this.transport.get(`/api/user/gdpr`);
-        return response;
-    }
 }
