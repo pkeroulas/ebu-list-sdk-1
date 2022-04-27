980f0388118043318f960fcc20aabdde93f1927d
diego <diego@bisect.pt>
2022-04-27 14:36:26 +0100

    Added new analysis type

--- lib/src/api/constants.ts
+++ lib/src/api/constants.ts
@@ -1,35 +1,36 @@
 export const analysisConstants = {
-  outcome: {
-      not_compliant: 'not_compliant',
-      compliant: 'compliant',
-      disabled: 'disabled'
-  },
-  errors: {
-      invalid_rtp_ts_vs_nt: 'errors.invalid_rtp_ts_vs_nt',
-  },
-  warnings: {
-      pcap: {
-          truncated: 'warnings.pcap.truncated',
-      },
-  },
-  analysesNames: {
-      rtp_ts_vs_nt: 'Delta between RTP timestamps and (N x Tframe) in ticks',
-      packet_ts_vs_rtp_ts: 'Delta between packet capture timestamps and RTP timestamps in ns',
-      inter_frame_rtp_ts_delta: 'Inter-frame RTP timestamps delta',
-      rtp_sequence: 'RTP sequence',
-      '2110_21_cinst': 'SMPTE 2110-21 (Cinst)',
-      '2110_21_vrx': 'SMPTE 2110-21 (VRX)',
-      tsdf: 'EBU TS-DF',
-      destination_multicast_mac_address: 'Destination Multicast MAC address',
-      destination_multicast_ip_address: 'Destination Multicast IP address',
-      unrelated_multicast_addresses: 'Multicast MAC and IP addresses mapping',
-      unique_multicast_destination_ip_address: 'Unique destination Multicast IP address',
-      ttml_time_base_is_media: 'TTML timeBase is "media"',
-      ttml_consistent_sequence_identifier: 'TTML consistent sequence identifier',
-      ttml_inconsistent_sequence_identifier: 'TTML inconsistent sequence identifier',
-      pkts_per_frame: 'RTP packets per frame/field',
-      marker_bit: 'RTP marker bit',
-      field_bits: 'Field bits',
-      anc_payloads: 'Ancillary payloads'
-  },
+    outcome: {
+        not_compliant: 'not_compliant',
+        compliant: 'compliant',
+        disabled: 'disabled'
+    },
+    errors: {
+        invalid_rtp_ts_vs_nt: 'errors.invalid_rtp_ts_vs_nt',
+    },
+    warnings: {
+        pcap: {
+            truncated: 'warnings.pcap.truncated',
+        },
+    },
+    analysesNames: {
+        rtp_ts_vs_nt: 'Delta between RTP timestamps and (N x Tframe) in ticks',
+        packet_ts_vs_rtp_ts: 'Delta between packet capture timestamps and RTP timestamps in ns',
+        inter_frame_rtp_ts_delta: 'Inter-frame RTP timestamps delta',
+        rtp_sequence: 'RTP sequence',
+        '2110_21_cinst': 'SMPTE 2110-21 (Cinst)',
+        '2110_21_vrx': 'SMPTE 2110-21 (VRX)',
+        tsdf: 'EBU TS-DF',
+        destination_multicast_mac_address: 'Destination Multicast MAC address',
+        destination_multicast_ip_address: 'Destination Multicast IP address',
+        unrelated_multicast_addresses: 'Multicast MAC and IP addresses mapping',
+        unique_multicast_destination_ip_address: 'Unique destination Multicast IP address',
+        ttml_time_base_is_media: 'TTML timeBase is "media"',
+        ttml_consistent_sequence_identifier: 'TTML consistent sequence identifier',
+        ttml_inconsistent_sequence_identifier: 'TTML inconsistent sequence identifier',
+        pkts_per_frame: 'RTP packets per frame/field',
+        marker_bit: 'RTP marker bit',
+        field_bits: 'Field bits',
+        anc_payloads: 'Ancillary payloads',
+        mac_address_analysis: "Repeated MAC addresses"
+    },
 };
